857cf9398d8d12a3b52a65503af79b7905f291a4
Theo Crevon <the@softwarehater.com>
2015-02-05 17:45:04 +0100

    Document interfaces

--- interfaces.go
+++ interfaces.go
@@ -1,25 +1,30 @@
 package main
 
+// Encrypter is the interface that wraps the basic Encrypt method
+//
+// Encrypt reads up plain data bytes contained in pd, encrypts
+// them and returns the resulting bytes as well as any potential errors.
+//
+// Implementations of Encrypter are let free to implement their own
+// Symmetric/Asymmetric algorithm needs management (public/private keys,
+// passphrase, etc...).
+// For a real-world implementation example, please refer to
+// serrure.openpgp.OpenPGPEncrypter or serrure.aes.AES256Encrypter.
 type Encrypter interface {
-	Encrypt([]byte) ([]byte, error)
+	Encrypt(pd []byte) (ed []byte, err error)
 }
 
+// Decrypter is the interface that wraps the basice Decrypt method
+//
+// Decrypts reads up the encrypted data bytes from ed,
+// decrypts them and returns the resulting plain data bytes as well
+// as any potential errors.
+//
+// Implementations of Decrypter are let free to implement their own
+// Symmetric/Asymmetric algorithm needs management (public/private keys,
+// passphrase, etc...)
+// For a real-world implementation example, please refer to
+// serrure.openpgp OpenPGPDecrypter or serrure.aes.AES256Decrypter.
 type Decrypter interface {
-	Decrypt([]byte) ([]byte, error)
-}
-
-type SymmetricEncrypter interface {
-	Encrypter
-}
-
-type SymmetricDecrypter interface {
-	Decrypter
-}
-
-type AsymmetricEncrypter interface {
-	Encrypter
-}
-
-type AsymmetricDecrypter interface {
-	Decrypter
+	Decrypt(ed []byte) (pd []byte, err error)
 }
